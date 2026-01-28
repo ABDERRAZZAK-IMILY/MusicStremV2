9b0c0e17fd56caf2378ac72ff2cbf82cfdcf4d13
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-01-28 10:25:43 +0100

    add Track interface to define audio track properties

---
+++ frontapp/src/app/models/track.model.ts
@@ -0,0 +1,13 @@
+export interface Track {
+    id: string;
+    title: string;
+    artist: string;
+    description?: string;
+    addedDate: string | Date;
+    duration: number;
+    genre: string;
+    audioFileUrl?: string;
+    coverImageUrl?: string;
+    audioFile?: File;       
+    coverImage?: File;         
+}
