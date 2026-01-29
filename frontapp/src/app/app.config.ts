b0598c3955a57c47aa811e42b7889abedd2b2738
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-01-29 16:04:08 +0100

    add Angular application configuration for HTTP client, state management, and enhanced routing features

--- frontapp/src/app/app.config.ts
+++ frontapp/src/app/app.config.ts
@@ -1,13 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, provideZonelessChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { trackReducer } from './core/store/track.reducer';
+import { TrackEffects } from './core/store/track.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideHttpClient(withFetch()),
+    provideStore({ tracks: trackReducer }),
+    provideEffects(TrackEffects)
   ]
 };
