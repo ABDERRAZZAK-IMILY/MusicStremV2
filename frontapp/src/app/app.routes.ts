109b1db5a6b3a493b0b20af7eceed260fd7ccad9
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-01-29 10:00:05 +0100

    add routing configuration for library and track management

--- frontapp/src/app/app.routes.ts
+++ frontapp/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'library', pathMatch: 'full' },
+  { 
+    path: 'library', 
+    loadComponent: () => import('./pages/library/library').then(m => m.Library) 
+  },
+  { 
+    path: 'add', 
+    loadComponent: () => import('./components/track-form/track-form').then(m => m.TrackFormComponent) 
+  },
+  { 
+    path: 'track/:id', 
+    loadComponent: () => import('./pages/track-detail/track-detail').then(m => m.TrackDetail) 
+  },
+  { 
+    path: 'edit/:id', 
+    loadComponent: () => import('./components/track-form/track-form').then(m => m.TrackFormComponent) 
+  },
+  { path: '**', redirectTo: 'library' }
+];
