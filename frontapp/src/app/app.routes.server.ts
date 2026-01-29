839d7a49b877ccdc57ed3e7a091644b2b8b193b0
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-01-29 10:00:13 +0100

    add server routes for track details and editing

--- frontapp/src/app/app.routes.server.ts
+++ frontapp/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'track/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
