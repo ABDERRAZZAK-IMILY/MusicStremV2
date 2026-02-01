8a8f9c3d97bc7922dd70642aeeb6de05146b9512
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-02-01 09:58:15 +0100

    Add Angular providers for zoneless change detection, HTTP client, and store in TrackService tests

--- frontapp/src/app/services/track.spec.ts
+++ frontapp/src/app/services/track.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { TrackService } from './track';
 
 describe('TrackService', () => {
   let service: TrackService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        TrackService,
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideStore({})
+      ]
+    });
     service = TestBed.inject(TrackService);
   });
 
