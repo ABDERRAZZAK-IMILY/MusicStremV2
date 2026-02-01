c2ae3154e4d7c627422d6e84bbbe2c6e52777b64
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-02-01 09:57:43 +0100

    Add getTrackById method to ITrackService interface

--- backend/src/main/java/com/musicstream/service/ITrackService.java
+++ backend/src/main/java/com/musicstream/service/ITrackService.java
@@ -9,5 +9,6 @@ public interface ITrackService {
    List<TrackDTO> getAllTracks();
    TrackDTO saveTrack(TrackDTO trackDto, org.springframework.web.multipart.MultipartFile audio, org.springframework.web.multipart.MultipartFile cover) throws Exception;
    void deleteTrack(String id);
+    TrackDTO getTrackById(String id);
 
 }
